ecb953dc3c7440c541f29279dd416547c5db645e
Ryan Indrajaya <sryan062@gmail.com>
2022-08-12 21:26:13 +0700

    first init

---
+++ consultio-frontend/pages/index.tsx
@@ -0,0 +1,21 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+
+const Home: NextPage = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
+        <h1>Consultio</h1>
+        <p>Selesaikan keluh kesahmu dengan berbagi.</p>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
